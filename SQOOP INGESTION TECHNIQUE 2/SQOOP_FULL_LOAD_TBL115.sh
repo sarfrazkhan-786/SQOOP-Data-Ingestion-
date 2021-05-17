ee21b545a609fe4d1b92e09daeeb0e43f11d941a
sarfrazkhan-786 <77966997+sarfrazkhan-786@users.noreply.github.com>
2021-05-17 12:04:16 +0530

    Add files via upload
    SQOOP INCREMENTAL AND FULL LOAD SECOND TECHNIQUE

---
+++ SQOOP INGESTION TECHNIQUE 2/SQOOP_FULL_LOAD_TBL115.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+function getimportalltables() {
+cat tables_1_115|while read LINE
+do
+  DBTABLE=${LINE}
+  sqoop import "-Dorg.apache.sqoop.splitter.allow_text_splitter=true" \
+  --connect 'jdbc:sqlserver://PRDASPSQL;database=DM_LIMS_PRD' \
+  --username lims_user_ro \
+  --password-file /user/reach_sakhan/dcl_labware/labware.passwrd \
+  --table ${LINE} \
+  --hive-delims-replacement "anything" \
+  --null-string '\\N' --null-non-string '\\N' \
+  --autoreset-to-one-mapper \
+  --fields-terminated-by '^' \
+  --hive-overwrite \
+  --hive-import \
+  --hive-table dm_labware_live_db.${LINE}_stg 
+ done
+}
+setProperties
+getimportalltables
---
+++ SQOOP INGESTION TECHNIQUE 2/SQOOP_INC_LOAD_JOBS.sh
@@ -0,0 +1,52 @@
+sqoop job --create INC_SAMPLE_APPEND \
+--meta-connect jdbc:hsqldb:hsql://prdbdsed01.dm.ae:16000/sqoop \
+-- import  --connect 'jdbc:sqlserver://PRDASPSQL;database=DM_LIMS_PRD' \
+--username 'lims_user_ro' --password-file '/user/reach_sakhan/dcl_labware/labware.passwrd' \
+--table 'SAMPLE' \
+--target-dir 'hdfs://nameservice1/user/hive/warehouse/dm_labware_live_db.db/sample_stg/' \
+--hive-delims-replacement "anything" \
+--null-string '\\N' --null-non-string '\\N' \
+--fields-terminated-by "^" \
+--incremental lastmodified \
+--check-column 'CHANGED_ON' \
+--last-value '2021-04-27 00:00:00' \
+--append \
+-m 10 \
+--verbose
+
+#-	TEST
+
+
+sqoop job --create INC_TEST_APPEND \
+--meta-connect jdbc:hsqldb:hsql://prdbdsed01.dm.ae:16000/sqoop \
+-- import  --connect 'jdbc:sqlserver://PRDASPSQL;database=DM_LIMS_PRD' \
+--username 'lims_user_ro' --password-file '/user/reach_sakhan/dcl_labware/labware.passwrd' \
+--table ‘TEST’ \
+--target-dir 'hdfs://nameservice1/user/hive/warehouse/dm_labware_live_db.db/sample_stg/' \
+--hive-delims-replacement "anything" \
+--null-string '\\N' --null-non-string '\\N' \
+--fields-terminated-by "^" \
+--incremental lastmodified \
+--check-column 'CHANGED_ON' \
+--last-value '2021-04-27 00:00:00' \
+--append \
+-m 10 \
+--verbose
+
+#           - RESULT 
+sqoop job --create INC_TEST_APPEND \
+--meta-connect jdbc:hsqldb:hsql://prdbdsed01.dm.ae:16000/sqoop \
+-- import  --connect 'jdbc:sqlserver://PRDASPSQL;database=DM_LIMS_PRD' \
+--username 'lims_user_ro' --password-file '/user/reach_sakhan/dcl_labware/labware.passwrd' \
+--table ‘RESULT \
+--target-dir 'hdfs://nameservice1/user/hive/warehouse/dm_labware_live_db.db/sample_stg/' \
+--hive-delims-replacement "anything" \
+--null-string '\\N' --null-non-string '\\N' \
+--fields-terminated-by "^" \
+--incremental lastmodified \
+--check-column 'CHANGED_ON' \
+--last-value '2021-04-27 00:00:00' \
+--append \
+-m 10 \
+--verbose
+
